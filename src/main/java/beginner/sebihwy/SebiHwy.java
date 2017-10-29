badc93d1c09e329101785516ae45beaa452e9953
Eric Smith <esmith@thebrighttag.com>
2017-10-29 15:15:11 -0500

    [Codechef: SEBIHWY] Started

---
+++ src/main/java/beginner/sebihwy/SebiHwy.java
@@ -0,0 +1,14 @@
+package beginner.sebihwy;
+
+/**
+ * This class contains logic for solving the
+ * "Sebi and the Highway" Codechef puzzle:
+ * <p>
+ * https://www.codechef.com/problems/SEBIHWY
+ */
+public class SebiHwy {
+
+  public String determineWinner(int[] params) {
+    return "SEBI";
+  }
+}
---
+++ src/test/java/beginner/sebihwy/SebiHwyTest.java
@@ -0,0 +1,10 @@
+package beginner.sebihwy;
+
+/**
+ * Tests the correctness of the solution for the "Sebi and the Highway"
+ * Codechef puzzle:
+ * <p>
+ * https://www.codechef.com/problems/SEBIHWY
+ */
+public class SebiHwyTest {
+}
